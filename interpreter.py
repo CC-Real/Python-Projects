92384690a6224b24fcfb8e449969f20ba39e76f8
CC-Real <danf26.cc@gmail.com>
2026-06-16 11:40:14 -0500

    Refactor input handling for arithmetic expressions

--- interpreter.py
+++ interpreter.py
@@ -1,3 +1,15 @@
 def main():
-    x = int(input("What numbers would you like to input? "))
-    
+    value = input("What would you like to input for your expression? ")
+    x, y, z = value.split(" ")
+    x = float(x)
+    z = float(z)
+    if y == "+":
+        result = x + z
+    elif y == "-":
+        result = x - z
+    elif y == "*":
+        result = x * z
+    elif y == "/":
+        result = x / z
+    print(f"{result:.1f}")
+main()
