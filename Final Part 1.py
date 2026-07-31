6d954b3d257b4f67a223d16995b538b098e634c4
CC-Real <danf26.cc@gmail.com>
2026-07-31 13:59:00 -0500

    Implement user name input and character count
    Add functionality to prompt for user's name and count characters.

--- Final Part 1.py
+++ Final Part 1.py
@@ -1,4 +1,7 @@
 #Part 1 of my programming course final 
+#Write Python code to:
+#• Ask the user to enter their name and store it in a string variable
+#• Print the number of characters in the user’s name to the output console
 def main():
     name = input("What is your name? ") #This just does the input for your name
     print(len(name)) #This is the function to count and print the amount of letters in your name
