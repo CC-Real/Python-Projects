7e88a87cde3c8166c42c3696e8b12c99274318c6
CC-Real <danf26.cc@gmail.com>
2026-07-31 13:57:44 -0500

    Implement printing of even integers between 10 and 30
    Add functionality to print even integers from 10 to 30.

--- Final Part 3.py
+++ Final Part 3.py
@@ -1,4 +1,5 @@
 #Part 3 of my programming course final
+#Print all even integers between 10 and 30 (inclusive) was the assigned task
 def main():
     for number in range(10,31):
         if number % 2 == 0:
